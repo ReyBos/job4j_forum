38158f92f848bc24191b6ee25319f4cd56249b04
reybos <andreybosiy@yandex.ru>
2021-04-07 14:40:39 +0300

    попытка починить сборку в Travis CI

--- src/test/java/ru/reybos/forum/control/IndexControlTest.java
+++ src/test/java/ru/reybos/forum/control/IndexControlTest.java
@@ -1,12 +1,18 @@
 package ru.reybos.forum.control;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.reybos.forum.Main;
+import ru.reybos.forum.model.Post;
+import ru.reybos.forum.service.PostService;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,9 +24,14 @@ public class IndexControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService postService;
+
     @Test
     @WithMockUser
     public void shouldReturnDefaultMessage() throws Exception {
+        List<Post> posts = List.of();
+        Mockito.when(postService.findAll()).thenReturn(posts);
         this.mockMvc.perform(get("/index"))
                 .andDo(print())
                 .andExpect(status().isOk())
